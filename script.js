212688ebb2b67253b917e3865d0bb19e472d0d16
krishnareddydev <krishnareddyqa@yahoo.com>
2024-08-20 18:50:03 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Automation fremwork Developer . . ." , "Python Programmer . . ." , "Web Developer . . ."],
+    typeSpeed: 70,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+})
